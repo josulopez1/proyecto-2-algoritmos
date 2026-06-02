4f64bace2850dbff23df90864f4c1895d5517de1
josulopez1 <jlopezvelasquez507@gmail.com>
2026-06-02 12:57:46 -0600

    Add Neo4jManager class for managing Neo4j database

---
+++ datafile/neo4j_driver.py
@@ -0,0 +1,37 @@
+from neo4j import GraphDatabase
+
+URI = "bolt://localhost:7687"
+USUARIO = "neo4j"
+PASSWORD = "password"  # cambia por la tuya
+
+
+class Neo4jManager:
+
+    def __init__(self):
+        self.driver = GraphDatabase.driver(
+            URI,
+            auth=(USUARIO, PASSWORD)
+        )
+
+    def cerrar(self):
+        self.driver.close()
+
+    def obtener_pagerank(self):
+
+        query = """
+        CALL gds.pageRank.stream('jugadoresGraph')
+        YIELD nodeId, score
+        RETURN gds.util.asNode(nodeId).nombre AS jugador, score
+        ORDER BY score DESC
+        """
+
+        with self.driver.session() as session:
+
+            resultado = session.run(query)
+
+            jugadores = {}
+
+            for row in resultado:
+                jugadores[row["jugador"]] = row["score"]
+
+            return jugadores
